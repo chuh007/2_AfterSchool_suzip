f806c6ae457f30cd765418b4d27ccfd4ae2f37ab
chuh007 <chhuh007@gmail.com>
2025-03-17 19:53:35 +0900

    add : TMP

---
+++ SuZipRPG_AfterSchool/Assets/100_Scripts/Test/TestScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        Debug.Log("<color=blue>Hello World!</color>");
+        Debug.LogWarning("Hello World!");
+        Debug.LogError("Hello World!");
+    }
+}
